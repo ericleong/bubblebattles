d1254837a100cc43219a4e0786948710199a151c
Eric Leong <leong@cooper.edu>
2012-09-30 03:12:50 -0400

    Added random color generator.

--- grid-server.js
+++ grid-server.js
@@ -12,9 +12,6 @@ var sids = new Array();
 var users = new Array();
 var kicked = new Array();
 
-var cyan = '#05FDFD';
-var orange = '#FA7005';
-var colors = [cyan, orange];
 var accepted_actions = ['move', 'speak', 'conn', 'info', 'thekick', 'theban'];
 var currentTime;
 var WORLD_W = 600,
@@ -74,7 +71,10 @@ io.sockets.on('connection', function(socket){
                 }));
             }
             request.radius = Math.random() * 10 + 4;
-            request.color = colors[0];
+            var rgb = hsvToRgb(10 * (Object.keys(sids).length * 743 % 36), 
+                5 * (Object.keys(sids).length * -343 % 4) + 80, 
+                5 * (Object.keys(sids).length * 233 % 8) + 40);
+            request.color = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
 
             socket.send(json({
                     action:'me',
@@ -164,3 +164,82 @@ io.sockets.on('connection', function(socket){
         }
     });
 });
+
+/**
+ * HSV to RGB color conversion
+ *
+ * H runs from 0 to 360 degrees
+ * S and V run from 0 to 100
+ * 
+ * Ported from the excellent java algorithm by Eugene Vishnevsky at:
+ * http://www.cs.rit.edu/~ncs/color/t_convert.html
+ */
+function hsvToRgb(h, s, v) {
+    var r, g, b;
+    var i;
+    var f, p, q, t;
+    
+    // Make sure our arguments stay in-range
+    h = Math.max(0, Math.min(360, h));
+    s = Math.max(0, Math.min(100, s));
+    v = Math.max(0, Math.min(100, v));
+    
+    // We accept saturation and value arguments from 0 to 100 because that's
+    // how Photoshop represents those values. Internally, however, the
+    // saturation and value are calculated from a range of 0 to 1. We make
+    // That conversion here.
+    s /= 100;
+    v /= 100;
+    
+    if(s == 0) {
+        // Achromatic (grey)
+        r = g = b = v;
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
+    
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
+
+    switch(i) {
+        case 0:
+            r = v;
+            g = t;
+            b = p;
+            break;
+            
+        case 1:
+            r = q;
+            g = v;
+            b = p;
+            break;
+            
+        case 2:
+            r = p;
+            g = v;
+            b = t;
+            break;
+            
+        case 3:
+            r = p;
+            g = q;
+            b = v;
+            break;
+            
+        case 4:
+            r = t;
+            g = p;
+            b = v;
+            break;
+            
+        default: // case 5:
+            r = v;
+            g = p;
+            b = q;
+    }
+    
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
